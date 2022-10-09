1cd25a5880fc1fd9cc6707ee9db118c71afe097c
Richards Cedeño <chards.rjcov@gmail.com>
2022-10-09 01:34:22 -0500

    First commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +9,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private emailComposer: EmailComposer) {}
+
+  enviar(){
+    //console.log("enviado");
+    let email = {
+      to: 'jose.cedenovillon@gmail.com',
+      cc: '',
+      bcc: [],
+      attachments: [],
+      subject: 'Email Composer',
+      body: 'El email ha sido enviado con éxito!',
+      isHtml: true,
+      app: "gmail"
+    }
+  
+    this.emailComposer.open(email);
+    //console.log(email);
+    //alert("Email Enviado");
+  }
 
 }
